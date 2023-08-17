71ad825934fbbd3707270e49ef98721e8c0ef347
teshanj7 <teshanjayakody217@gmail.com>
2023-08-17 17:01:31 +0530

    commit

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,14 +1,19 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
-    products: any[] = [
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -30,4 +35,8 @@ export class ProductListComponent{
             "imageUrl": "assets/images/hammer.png"
           }
     ];
+
+    toggleImage(): void{
+      this.showImage = !this.showImage;
+    }
 }
