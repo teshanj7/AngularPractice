af46546d23224346da6ef92479f1ef07f4db8f42
teshanj7 <teshanjayakody217@gmail.com>
2023-08-18 11:06:13 +0530

    friday 1st commit

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -7,12 +7,25 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter: string =' ';
+
+    get listFilter() : string{
+      return this._listFilter;
+    }
+
+    set listFilter(value: string){
+      this._listFilter = value;
+      this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts: IProduct[] = [];
+
     products: IProduct[] = [
         {
             "productId": 2,
@@ -39,4 +52,16 @@ export class ProductListComponent{
     toggleImage(): void{
       this.showImage = !this.showImage;
     }
+
+    ngOnInit(): void {
+      this._listFilter = 'cart'
+    }
+
+    performFilter(filterBy: string) : IProduct[]{
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
+
 }
