57a6329408cb4e7757d0e0f4e2b77d08edb3d1cc
teshanj7 <teshanjayakody217@gmail.com>
2023-08-18 16:59:09 +0530

    friday commit

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -12,6 +13,10 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+
+    constructor(private productService: ProductService){
+      
+    }
 
     private _listFilter: string =' ';
 
@@ -26,41 +31,25 @@ export class ProductListComponent implements OnInit{
 
     filteredProducts: IProduct[] = [];
 
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
+    products: IProduct[] = [];
 
     toggleImage(): void{
       this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-      this._listFilter = 'cart'
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string) : IProduct[]{
       filterBy = filterBy.toLocaleLowerCase();
       return this.products.filter((product: IProduct) =>
         product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
+    onRatingClicked(message: string) : void{
+      this.pageTitle = 'Product List: ' + message;
     }
 
 
